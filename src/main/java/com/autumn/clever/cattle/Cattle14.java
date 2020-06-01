27cbe2ec6d093e6b35d101531366c6b0a913709e
张秋颖 <zhangqiuying02@maoyan.com>
2020-06-01 17:48:38 +0800

    链表 输出倒数第k个node

---
+++ src/main/java/com/autumn/clever/cattle/Cattle14.java
@@ -0,0 +1,26 @@
+package com.autumn.clever.cattle;
+
+/**
+ * 输入一个链表，输出该链表中倒数第k个结点。
+ *
+ * @Author: zhangqiuying
+ * @Date: 2020/5/31 22:14
+ */
+public class Cattle14 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode FindKthToTail(ListNode head, int k) {
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
