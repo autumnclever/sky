a9ac2b5bb909475e9f1e8e4456336d404253800a
张秋颖 <zhangqiuying02@maoyan.com>
2020-06-01 17:49:29 +0800

    输出倒数第k个listNode

--- src/main/java/com/autumn/clever/cattle/Cattle14.java
+++ src/main/java/com/autumn/clever/cattle/Cattle14.java
@@ -8,11 +8,47 @@ package com.autumn.clever.cattle;
  */
 public class Cattle14 {
     public static void main(String[] args) {
-
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
+        ListNode node6 = new ListNode(7);
+        ListNode node7 = new ListNode(8);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        ListNode result = FindKthToTail(head, 8);
+        System.out.println(result);
     }
 
-    public ListNode FindKthToTail(ListNode head, int k) {
-        return null;
+    public static ListNode FindKthToTail(ListNode head, int k) {
+        if (head == null || k <= 0) {
+            return null;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        while (k-- > 1) {
+            if (p1.next != null) {
+                p1 = p1.next;
+            } else {
+                return null;
+            }
+        }
+
+        while (p1.next != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+
+        return p2;
     }
 }
 
@@ -22,5 +58,13 @@ class ListNode {
 
     ListNode(int val) {
         this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
     }
 }
